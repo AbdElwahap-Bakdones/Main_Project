28496c5e8946452f2745102e4c5ee7c5a17e5bb7
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2022-10-19 19:53:56 +0300

    config graphene

--- Schema/Schema.py
+++ Schema/Schema.py
@@ -18,15 +18,27 @@ class CarsCategory(DjangoObjectType):
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
     all_Compani = graphene.List(CompaniCategory)
-    Cars_by_nam = graphene.Field(Cars, name=graphene.String(required=True))
-    Cars_by_Company = graphene.Field(
-        Cars, Company_name=graphene.String(required=True))
+    Cars_by_nam = graphene.Field(
+        CarsCategory, name=graphene.String(required=True))
+    Cars_by_Company = graphene.List(
+        CarsCategory, compani=graphene.Int(required=True))
 
     def resolve_all_Compani(root, info):
         try:
             return Compani.objects.all()
         except:
             return Compani.DoesNotExist()
+
+    def resolve_Cars_by_nam(root, info, name):
+        print(name)
+        try:
+            return Cars.objects.get(name=name)
+        except:
+            return Compani.DoesNotExist()
+
+    def resolve_Cars_by_Company(root, info, compani):
+        print(compani)
+        return Cars.objects.filter(compani=compani)
 
 
 schema = graphene.Schema(query=Query)
