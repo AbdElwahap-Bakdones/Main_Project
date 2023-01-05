9f79b437774e6504fc0dfaf1a44aafca5a2cd2c0
ghaithsh <ghaithshpro@gmail.com>
2023-01-05 16:04:10 +0200

    section CRUD

--- Graphql/Mutation/club.py
+++ Graphql/Mutation/club.py
@@ -4,9 +4,9 @@ import graphene
 from core import models, serializer
 from ..Auth.permission import checkPermission
 from rest_framework import status as status_code
+from .. import QueryStructure
 from abc import ABC, abstractmethod, abstractclassmethod
 from django.http import HttpResponse
-from .. import QueryStructure
 
 
 class AddClub  (graphene.Mutation, QueryStructure.Attributes):
@@ -31,25 +31,25 @@ class AddClub  (graphene.Mutation, QueryStructure.Attributes):
                 seria.validated_data
                 msg = seria.errors
                 status = status_code.HTTP_201_CREATED
-                club = seria.save()
+                data = seria.save()
             else:
                 msg = seria.errors
-                club = None
+                data = None
                 status = status_code.HTTP_406_NOT_ACCEPTABLE
         except Exception as e:
             print('Error in AddClub :')
             print(e)
             msg = str(e)
-            club = None
+            data = None
             status = status_code.HTTP_500_INTERNAL_SERVER_ERROR
-        return QueryStructure.MyReturn(instanse=self, data=club, message=msg, code=status)
+        return QueryStructure.MyReturn(instanse=self, data=data, message=msg, code=status)
 
 
 class UpdateClub(graphene.Mutation, QueryStructure.Attributes):
     data = graphene.Field(typeobject.ClubObjectType)
 
     class Arguments:
-        data = inputtype.InputUpdateClub()
+        ClubData = inputtype.UpdateClubInput()
 
     @classmethod
     def mutate(self, root, info, **kwargs):
@@ -57,22 +57,66 @@ class UpdateClub(graphene.Mutation, QueryStructure.Attributes):
             user = models.User(info.context.META["user"])
             if not checkPermission("core.change_club", user.pk):
                 return QueryStructure.MyReturn(self, None, 'You do not have permission to complete the process', status_code.HTTP_401_UNAUTHORIZED)
-            data = kwargs['data']
-            club_object = models.Club.objects.get(
-                id=data['id'], is_deleted=False)
-            # club_object = models.Club.objects.filter(
-            #     pk=data['id'], is_deleted=False)
-            # if not club_object.exists():
-            #     msg = 'there is no club with id='+data['id']
-            #     club = None
-            #     status = status_code.HTTP_404_NOT_FOUND
-            #     return QueryStructure.MyReturn(self, None, msg, status)
+            data = kwargs['ClubData']
+            # club_object = models.Club.objects.get(
+            #     id=data['id'], is_deleted=False)
+            club_object = models.Club.objects.filter(
+                pk=data['id'], is_deleted=False)
+            if not club_object.exists():
+                msg = 'there is no club with id='+data['id']
+                club = None
+                status = status_code.HTTP_404_NOT_FOUND
+                return QueryStructure.MyReturn(self, None, msg, status)
 
             seria = serializer.ClubSerializer(
-                club_object, data=data, partial=True)
+                club_object.first(), data=data, partial=True)
             if seria.is_valid():
                 seria.validated_data
                 msg = 'updated!'
+                status = status_code.HTTP_200_OK
+                data = seria.save()
+            else:
+                msg = seria.errors
+                data = None
+                status = status_code.HTTP_406_NOT_ACCEPTABLE
+        except Exception as e:
+            print('Error in UpdateClub')
+            print(e)
+            msg = str(e)
+            data = None
+            status = status_code.HTTP_500_INTERNAL_SERVER_ERROR
+        return QueryStructure.MyReturn(instanse=self, data=data, message=msg, code=status)
+
+
+class DeleteClub(graphene.Mutation, QueryStructure.Attributes):
+    data = graphene.Field(typeobject.ClubObjectType)
+
+    class Arguments:
+        ClubData = inputtype.DeleteClubInput()
+
+    @classmethod
+    def mutate(self, root, info, **kwargs):
+        try:
+            user = models.User(info.context.META["user"])
+            if not checkPermission("core.delete_club", user.pk):
+                return QueryStructure.MyReturn(self, None, 'You do not have permission to complete the process', status_code.HTTP_401_UNAUTHORIZED)
+            # club_object = models.Club.objects.get(
+            #     id=data['id'], is_deleted=False)
+            data = kwargs['ClubData']
+            club_object = models.Club.objects.filter(
+                pk=data["id"], is_deleted=False)
+            if not club_object.exists():
+                msg = 'there is no club with id='+data['id']
+                club = None
+                status = status_code.HTTP_404_NOT_FOUND
+                return QueryStructure.MyReturn(self, None, msg, status)
+
+            data.update({"is_deleted": True})
+            seria = serializer.ClubSerializer(
+                club_object.first(), data=data, partial=True)
+            if seria.is_valid():
+                seria.validated_data
+                msg = 'deleted!'
                 status = status_code.HTTP_200_OK
                 club = seria.save()
             else:
--- Graphql/Mutation/section.py
+++ Graphql/Mutation/section.py
@@ -1,74 +1,124 @@
-from ..ModelsGraphQL import typeobject
+from graphene_django.rest_framework.mutation import SerializerMutation
+from ..ModelsGraphQL import typeobject, inputtype
 import graphene
 from core import models, serializer
 from ..Auth.permission import checkPermission
+from rest_framework import status as status_code
+from .. import QueryStructure
+from abc import ABC, abstractmethod, abstractclassmethod
+from django.http import HttpResponse
 
 
-# def checkmyclub(idClub: id, self: object, info: object):
-#     myclub = models.Club.objects.filter(manager_id=models.Manager.objects.get(
-#         user_id=info.context.META["user"])).first()
-#     if not myclub:
-#         return self(Section=None, message="this not your club", status=401)
+class AddSection  (graphene.Mutation, QueryStructure.Attributes):
 
-
-class SectionInput(graphene.InputObjectType):
-    name = graphene.String(required=True)
-    club_id = graphene.ID(required=True)
-    is_available = graphene.Boolean(required=True)
-
-
-class AddSection(graphene.Mutation):
-    section = graphene.Field(typeobject.SectionObjectType)
-    message = graphene.String()
-    status = graphene.Int()
+    data = graphene.Field(typeobject.SectionObjectType)
 
     class Arguments:
-        SectionData = SectionInput()
+        SectionData = inputtype.AddSectionInput()
 
     @classmethod
     def mutate(self, root, info, **kwargs):
-        checkPermission("core.add_section", info)
-        # checkmyclub(kwargs["SectionData"]["club_id"], self, info)
-        kwargs["SectionData"].update({"sub_manager_id": models.SubManager.objects.get(
-            user_id=info.context.META["user"]).pk})
-        seria = serializer.SectionSerializer(
-            data=kwargs["SectionData"])
-        if seria.is_valid():
-            seria.validated_data
-            msg = seria.errors
-            status = 200
-            section = seria.save()
-        else:
-            msg = seria.errors
-            section = None
-            status = 400
-        return self(section=section, message=msg, status=status)
+        try:
+            user = info.context.META["user"]
+            if not checkPermission("core.add_section", user):
+                return QueryStructure.MyReturn(self, None, 'You do not have permission to complete the process', status_code.HTTP_401_UNAUTHORIZED)
+            seria = serializer.SectionSerializer(data=kwargs["SectionData"])
+            if seria.is_valid():
+                seria.validated_data
+                msg = seria.errors
+                status = status_code.HTTP_201_CREATED
+                data = seria.save()
+            else:
+                msg = seria.errors
+                data = None
+                status = status_code.HTTP_406_NOT_ACCEPTABLE
+        except Exception as e:
+            print('Error in AddSection :')
+            print(e)
+            msg = str(e)
+            data = None
+            status = status_code.HTTP_500_INTERNAL_SERVER_ERROR
+        return QueryStructure.MyReturn(instanse=self, data=data, message=msg, code=status)
 
 
-class UpdatSection(graphene.Mutation):
-    section = graphene.Field(typeobject.SectionObjectType)
-    message = graphene.String()
-    status = graphene.Int()
+class UpdateSection(graphene.Mutation, QueryStructure.Attributes):
+    data = graphene.Field(typeobject.SectionObjectType)
 
     class Arguments:
-        id = graphene.ID(required=True)
-        name = graphene.String()
-        sub_manager_id = graphene.ID()
-        is_available = graphene.Boolean()
+        SectionData = inputtype.UpdateSectionInput()
 
     @classmethod
-    def mutate(self, root, info, id, **kwargs):
-        checkPermission("core.change_section", info)
-        sub = models.Section.objects.get(id=id)
-        seria = serializer.SectionSerializer(sub,
-                                             data=kwargs, partial=True)
-        if seria.is_valid():
-            seria.validated_data
-            msg = seria.errors
-            status = 200
-            section = seria.save()
-        else:
-            msg = seria.errors
-            section = None
-            status = 400
-        return self(section=section, message=msg, status=status)
+    def mutate(self, root, info, **kwargs):
+        try:
+            user = models.User(info.context.META["user"])
+            if not checkPermission("core.change_section", user.pk):
+                return QueryStructure.MyReturn(self, None, 'You do not have permission to complete the process', status_code.HTTP_401_UNAUTHORIZED)
+            data = kwargs['SectionData']
+            Section_object = models.Section.objects.filter(
+                pk=data['id'], is_deleted=False)
+            if not Section_object.exists():
+                msg = 'there is no Section with id='+data['id']
+                data = None
+                status = status_code.HTTP_404_NOT_FOUND
+                return QueryStructure.MyReturn(self, None, msg, status)
+
+            seria = serializer.SectionSerializer(
+                Section_object.first(), data=data, partial=True)
+            if seria.is_valid():
+                seria.validated_data
+                msg = 'updated!'
+                status = status_code.HTTP_200_OK
+                data = seria.save()
+            else:
+                msg = seria.errors
+                data = None
+                status = status_code.HTTP_406_NOT_ACCEPTABLE
+        except Exception as e:
+            print('Error in UpdateSection')
+            print(e)
+            msg = str(e)
+            data = None
+            status = status_code.HTTP_500_INTERNAL_SERVER_ERROR
+        return QueryStructure.MyReturn(instanse=self, data=data, message=msg, code=status)
+
+
+class DeleteSection(graphene.Mutation, QueryStructure.Attributes):
+    data = graphene.Field(typeobject.SectionObjectType)
+
+    class Arguments:
+        SectionData = inputtype.DeleteSectionInput()
+
+    @classmethod
+    def mutate(self, root, info, **kwargs):
+        try:
+            user = models.User(info.context.META["user"])
+            if not checkPermission("core.delete_section", user.pk):
+                return QueryStructure.MyReturn(self, None, 'You do not have permission to complete the process', status_code.HTTP_401_UNAUTHORIZED)
+            data = kwargs['SectionData']
+            Section_object = models.Section.objects.filter(
+                pk=data["id"], is_deleted=False)
+            if not Section_object.exists():
+                msg = 'there is no Section with id='+data['id']
+                Section = None
+                status = status_code.HTTP_404_NOT_FOUND
+                return QueryStructure.MyReturn(self, None, msg, status)
+
+            data.update({"is_deleted": True})
+            seria = serializer.SectionSerializer(
+                Section_object.first(), data=data, partial=True)
+            if seria.is_valid():
+                seria.validated_data
+                msg = 'deleted!'
+                status = status_code.HTTP_200_OK
+                Section = seria.save()
+            else:
+                msg = seria.errors
+                Section = None
+                status = status_code.HTTP_406_NOT_ACCEPTABLE
+        except Exception as e:
+            print('Error in UpdateSection')
+            print(e)
+            msg = str(e)
+            Section = None
+            status = status_code.HTTP_500_INTERNAL_SERVER_ERROR
+        return QueryStructure.MyReturn(instanse=self, data=Section, message=msg, code=status)
