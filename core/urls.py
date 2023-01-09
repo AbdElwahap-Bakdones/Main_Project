f6c6e6d86b7136eaad602da92edbfa55d16e0831
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-01-09 15:41:43 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
 urlpatterns = [
-    path('delete_user', views.deleteUser)
+    path('login_view', views.login_view),
+    path('csrf', views.csrf)
+
 ]
