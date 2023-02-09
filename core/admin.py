2cf2b7953c014e2a8c52d6a6fee13658d7027e0d
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-02-09 17:53:20 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from django.apps import apps
-from .models import User
+from . import models
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # admin.site.register(User)
 app = apps.get_app_config('core')
 for model_name, model in app.models.items():
-    if model == User:
+    if model == models.User or model == models.Friend:
         continue
     admin.site.register(model)
 
 
-@admin.register(User)
+@admin.register(models.Friend)
+class FrindAdmin(admin.ModelAdmin):
+    list_display = ['player1', 'player2', 'state', 'sender']
+
+
+@admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
     pass
 
