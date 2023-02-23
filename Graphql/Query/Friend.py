ae4d94033e16e40d7f8bc31483823f9602700684
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-02-23 18:40:01 +0300

    Update Friend.py

--- Graphql/Query/Friend.py
+++ Graphql/Query/Friend.py
@@ -8,27 +8,17 @@ from django.db.models import Q
 import graphene
 
 
-def correct_structure(data, user):
-    for i in data:
-        if i.player2.user_id == user:
-            player = i.player2
-            i.player2 = i.player1
-            i.player1 = player
-
-
 class AllFriend(ObjectType, QueryFields):
     data = relay.ConnectionField(
         relays.FriendConnection)
 
     def resolve_data(root, info, **kwargs):
-        print(kwargs)
         user = info.context.META['user']
         if not QueryFields.is_valide(info, user, 'core.view_friend'):
             return QueryFields.rise_error(user)
 
         data = Friend.objects.filter(
             Q(player1__user_id=user) & Q(state="accepted"))
-        print(data[1])
         return QueryFields.OK(info, data=data)
 
 
@@ -49,12 +39,8 @@ class GetFriendByName(ObjectType, QueryFields):
 
         )
         if not data.exists():
-            QueryFields.set_extra_data(
-                user, status_code.HTTP_404_NOT_FOUND, 'not exists')
-            return []
-        QueryFields.set_extra_data(user, status_code.HTTP_200_OK, 'ok')
-        correct_structure(data, user)
-        return data
+            return QueryFields.NotFound(info=info)
+        return QueryFields.OK(info=info, data=data)
 
 
 class GetFriendById(ObjectType, QueryFields):
@@ -66,15 +52,13 @@ class GetFriendById(ObjectType, QueryFields):
         user = info.context.META['user']
         if not QueryFields.is_valide(info, user, 'core.view_friend'):
             return QueryFields.rise_error(user)
-        data = Friend.objects.filter(
-            player2__pk=kwargs['player_id'], state='accepted')
+        data = Friend.objects.filter(player1__user_id=user,
+                                     player2__pk=kwargs['player_id'], state='accepted')
+        print(data)
         if not data.exists():
-            QueryFields.set_extra_data(
-                user, status_code.HTTP_404_NOT_FOUND, 'not exists')
-            return []
-        QueryFields.set_extra_data(user, status_code.HTTP_200_OK, 'ok')
-        correct_structure(data, user)
-        return data
+            print('nooonnono')
+            return QueryFields.NotFound(info=info)
+        return QueryFields.OK(info=info, data=data)
 
 
 class GetFriendCanAddToTeam(ObjectType, QueryFields):
