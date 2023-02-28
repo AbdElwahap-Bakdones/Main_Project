fb73debb78dfd624c312f0fdd5ceb75241bd8800
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-02-28 17:30:28 +0300

    Update Duration.py

--- Graphql/Query/Duration.py
+++ Graphql/Query/Duration.py
@@ -56,10 +56,7 @@ class AvailableDurationByStadium(ObjectType, QueryFields):
             avlaible_duration = avlaible_duration.annotate(available=Value(
                 True, output_field=MODELS.BooleanField()))
             all_duration = list(chain(NAV_duration, avlaible_duration))
-            print(all_duration)
-            if avlaible_duration.count() > 0:
-                return QueryFields.OK(info=info, data=all_duration)
-            return QueryFields.NotFound(info=info, msg='Sorry the date you have chosen is complete')
+            return QueryFields.OK(info=info, data=all_duration)
         except Exception as e:
             print('Error in AvailableDurationByStadium')
             print(str(e))
