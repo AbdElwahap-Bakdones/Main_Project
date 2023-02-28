08482311f4783281d16c0b520fc199e2835c945b
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-02-28 20:13:38 +0300

    edit Geo

--- core/Geo.py
+++ core/Geo.py
@@ -6,7 +6,7 @@ def set_point_field(validated_data):
     try:
         old_validated_data = validated_data
         pnt = GEOSGeometry(
-            'POINT('+validated_data['location_lat']+' '+validated_data['location_long']+')', srid=4326)
+            'POINT('+validated_data['location_lat']+' '+validated_data['location_long']+')', srid=32140)
         validated_data['point'] = pnt
         return validated_data
     except Exception as e:
