a2e43ac7b2dee5322c3102718d7f92955e68d1c5
AbdElwahap-Bakdones <abdaelwahapbak@gmail.com>
2023-03-04 11:45:05 +0300

    Update views.py

--- Bank/views.py
+++ Bank/views.py
@@ -9,6 +9,7 @@ def deposit(client, amount):
         with transaction.atomic():
             balance = MODELSBANK.Account.objects.select_for_update().get(
                 client_name=client)
+            print(f' bank account id {balance.pk}')
             balance.client_ammunt = balance.client_ammunt+amount
             balance.save()
             return balance.client_ammunt
